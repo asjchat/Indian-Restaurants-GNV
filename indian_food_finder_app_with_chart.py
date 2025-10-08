11c4d4ed1ac669da422ee8f1b6836dafc77e4643
Ananda <63558278+asjchat@users.noreply.github.com>
2025-10-08 13:16:45 -0400

    Update indian_food_finder_app_with_chart.py

--- indian_food_finder_app_with_chart.py
+++ indian_food_finder_app_with_chart.py
@@ -1,9 +1,6 @@
 
 import streamlit as st
 import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 import numpy as np
 from scipy import stats
 from sklearn.linear_model import LinearRegression
